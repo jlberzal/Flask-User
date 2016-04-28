2acf3dd43fd197d74fcb07b7b8c7e9b5db060b88
Jose Luis Berzal <joseluisberzal@Joses-MacBook-Air.local>
2016-04-28 23:29:37 +0200

    my first commit

--- runserver.py
+++ runserver.py
@@ -2,5 +2,12 @@ from example_apps.basic_app import create_app
 
 app = create_app()
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+# app.run(host='0.0.0.0', port=5000, debug=True)
 
